67c3c559eadea0588d1fad5a326486e8d8b7cea0
Vinay Singh <vinaysingh8866@gmail.com>
2022-12-15 14:56:02 -0500

    removed backed form folder

--- mosaique-decentralized-workflows-1/js-parser/src/app.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class AppService {
-  getHello(): string {
-    return 'REST API for Mosaique Node.js Parser and Template Handler';
-  }
-}
